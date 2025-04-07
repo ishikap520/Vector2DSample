95d29ad87e4bf7675f9a992e058e4af5745901c9
ishikap520 <ishikap520@gmail.com>
2025-04-07 09:02:52 -0500

    Vector2DSample.cpp

--- Vector2DSample/Vector2DSample.cpp
+++ Vector2DSample/Vector2DSample.cpp
@@ -29,3 +29,38 @@ int main()
         cout << endl;
     }
 }
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double sales;
+    double ts = 0;
+    vector<double> temp;
+    vector<vector<double>> Sales;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cout << "Enter the Sales for Row # " << (i + 1) << " Column # " << (j + 1) << " ==> ";
+            cin >> sales;
+            ts += sales;
+            temp.push_back(sales);
+        }
+        cout << endl;
+        temp.push_back(ts);
+        Sales.push_back(temp);
+        temp.clear();
+        ts = 0;
+    }
+    //Printing
+    for (int i = 0; i < Sales.size(); i++)
+    {
+        for (int j = 0; j < Sales[i].size(); j++)
+        {
+            cout << Sales[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
