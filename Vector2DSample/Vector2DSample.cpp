4a87ebbe15a914ca60ddebeed98c3f3dc9bb3998
ishikap520 <ishikap520@gmail.com>
2025-09-17 15:37:36 -0500

    Vector2DSample.cpp

--- Vector2DSample/Vector2DSample.cpp
+++ Vector2DSample/Vector2DSample.cpp
@@ -4,37 +4,6 @@ using namespace std;
 
 int main()
 {
-    int y;
-    vector<int> temp;
-    vector<vector<int>> years;
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 5; j++)
-        {
-            cout << "Enter the year for Row # " << (i + 1) << " Column # " << (j + 1) << " ==> ";
-            cin >> y;
-            temp.push_back(y);
-        }
-        cout << endl;
-        years.push_back(temp);
-        temp.clear();
-    }
-    //Printing
-    for (int i = 0; i < years.size(); i++)
-    {
-        for (int j = 0; j < years[i].size(); j++)
-        {
-            cout << years[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-#include <vector>
-#include <iostream>
-using namespace std;
-
-int main()
-{
     double sales;
     double ts = 0;
     vector<double> temp;
